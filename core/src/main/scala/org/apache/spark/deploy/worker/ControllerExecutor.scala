0f1608b4a42f2d8604e817de9905f309ba5e87ad
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-06-05 14:46:56 +0200

    Controller Executor revert to correct version

--- core/src/main/scala/org/apache/spark/deploy/worker/ControllerExecutor.scala
+++ core/src/main/scala/org/apache/spark/deploy/worker/ControllerExecutor.scala
@@ -37,10 +37,8 @@ class ControllerExecutor
   val tasks: Double = _tasks.toDouble
   var worker: Worker = _
 
-  var csiOld: Double = core.toDouble
   var SP: Double = 0.0
   var completedTasks: Double = 0.0
-  var cs: Double = 0.0
 
   val timer = new Timer()
   var oldCore = core
@@ -76,37 +74,24 @@ class ControllerExecutor
     timer.cancel()
   }
 
-  def nextAllocation(statx: Int = 3): Double = {
-//    // err = setpoint - real
-//    val err = SP - (completedTasks / tasks)
-//    val xc = xc_old + (Ts.toDouble / Ti) * err_old
-//    var c = K * xc + K * err
-//
-//    if (c < coreMin)
-//      c = coreMin
-//
-//    // send c to pollon and receive corrected value
-//    c = worker.pollon.fix_cores(appId, stageId, c)
-//
-//    err_old = err
-////    xc_old = (c / K) - err
-//    xc_old = c - (K * err)
-//    // apply c
-//    c
+  def nextAllocation(): Double = {
+    // err = setpoint - real
+    val err = SP - (completedTasks / tasks)
+    val xc = xc_old + (Ts.toDouble / Ti) * err_old
+    var c = math.max(coreMin.toDouble, K * xc + K * err)
 
-    val csp = K * (SP - (completedTasks / tasks))
-    if (statx != 3) {
-      cs = coreMin
-    }
-    else {
-      val csi = csiOld + K * (Ts.toDouble / Ti) * (SP - (completedTasks / tasks))
-      cs = math.max(coreMin.toDouble, csp + csi)
+    // send c to pollon and receive corrected value
+    c = worker.pollon.fix_cores(appId, stageId, c)
 
-      cs = worker.pollon.fix_cores(appId, stageId, cs)
-    }
-    cs = math.ceil(cs / CQ) * CQ
-    csiOld = (cs - csp) / K
-    cs
+    // make c proportional to Core Quantum
+    c = math.ceil(c / CQ) * CQ
+
+    // update values for next iteration
+    err_old = err
+    xc_old = (c / K) - err
+
+    // apply c
+    c
 
   }
 
