e5a00a2ea80d9fff07ba9ee44ad470d3ae036f49
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-07-11 14:41:33 +0200

    Limit assigned cores to number of available tasks

--- core/src/main/scala/org/apache/spark/deploy/worker/ControllerExecutor.scala
+++ core/src/main/scala/org/apache/spark/deploy/worker/ControllerExecutor.scala
@@ -83,7 +83,7 @@ class ControllerExecutor
     }
     else {
       val csi = csiOld + K * (Ts.toDouble / Ti) * (SP - (completedTasks / tasks))
-      cs = math.min(coreMax.toDouble, math.max(coreMin.toDouble, csp + csi))
+      cs = math.min(math.min(coreMax.toDouble, math.max(coreMin.toDouble, csp + csi)), tasks-completedTasks)
     }
     cs = math.ceil(cs / CQ) * CQ
     csiOld = cs - csp
